aaee1f61b98bc698b69736488fbdb472fb92c76b
viktoriiadron <88160860+viktoriiadron@users.noreply.github.com>
2021-08-12 10:07:16 +0300

    updates after checking

--- 22.Prototype/script.js
+++ 22.Prototype/script.js
@@ -8,9 +8,10 @@ let student = {
         return `${this.firstName} ${this.lastName}`
     },
     isGrantHolder() {
-        if (this.averageScore >= 4) {
-            return true
-        } else return false
+        return this.averageScore >= 4;
+        // if (this.averageScore >= 4) {
+        //     return true
+        // } else return false
     }
 }
 
@@ -20,7 +21,7 @@ function Aspirant(dissertationTopic, isDissertationComplete) {
     this.dissertationTopic = dissertationTopic;
     this.isDissertationComplete = isDissertationComplete;
     this.isGrantHolder = function () {
-        if (this.avarageScore >= 4.5 && this.isDissertationComplete === true) {
+        if (this.averageScore >= 4.5 && this.isDissertationComplete) {
             return true
        } else return false
     }
@@ -35,16 +36,19 @@ console.log(aspirant.isGrantHolder());
 //     constructor(dissertationTopic, isDissertationComplete) {
 //         this.dissertationTopic = dissertationTopic;
 //         this.isDissertationComplete = isDissertationComplete;
+//         this.__proto__ = student;
 //     }
-//         isGrantHolder() { 
-//         if (this.avarageScore >= 4.5 && this.isDissertationComplete === true) {
-//             return true
-//         } else return false
+//     isGrantHolder() {
+//             return this.avarageScore >= 4.5 && this.isDissertationComplete
+//         // if (this.averageScore >= 4.5 && this.isDissertationComplete === true) {
+//         //     return true
+//         // } else return false
 //     }    
 // }
 
-// let aspirant = new Aspirant('some topic', true);
-// aspirant.__proto__ = student;
+// let aspirant = new Aspirant('some topic', false);
+// //aspirant.__proto__ = student;
+// console.log(aspirant)
 // console.log(aspirant.isGrantHolder());
 // console.log(aspirant.getFullName());
 
@@ -66,11 +70,12 @@ class Plane {
 let airport = {
     planes: [],
     getFlyingPlanes() {
-        let flying = this.planes.filter(function isFlyingFunc(plane) {
-            return plane.isFlying === true;
-        });
-        console.log(flying)
-        return flying.length
+        // let flying = this.planes.filter(function isFlyingFunc(plane) {
+        //     return plane.isFlying === true;
+        // });
+        // //console.log(flying)
+        // return flying.length
+        return this.planes.filter(plane => plane.isFlying)
     }
 }
 
