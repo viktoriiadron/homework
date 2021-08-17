1b522bd20fa39a27a0a2e25039d596cd27fcff16
viktoriiadron <88160860+viktoriiadron@users.noreply.github.com>
2021-08-17 12:15:20 +0300

    function modified

--- 24.Promises/script.js
+++ 24.Promises/script.js
@@ -1,32 +1,25 @@
-function getNum() {
+function newGame() {
     let promise = new Promise(function (resolve, reject) {
         setTimeout(function () {
             resolve(Math.floor(Math.random() * 6 + 1));
         }, 2000);
     });
-    return promise;
-}
 
-async function newGame() {
-    let number = await getNum();
-
-    let promice = new Promise(function (resolve, reject) {
-       // console.log(number);
-        
-        if (number >= 1 && number <= 5) {
-            resolve(console.log(`Start the game ${number}`));
-        } else reject(console.log("Exit"));
-    });
-    promice
-        .then(function () {
-         if (number === 1) {
-             console.log('Stay here');
-         } else if (number >= 2) {
-             console.log(`Go ${number} steps`)
+    promise
+        .then(function (number) {
+            if (number >= 1 && number <= 5) {
+                console.log(`Start the game ${number}`);
+                return number;
             }
+            else (reject());
         })
-    .catch(error => {console.error(`${error} Exit`)})
+        .then(function (number) {
+            if (number === 1) {
+                console.log('Stay here');
+            } else { console.log(`Go ${number} steps`) }
+        })
+        .catch(err => console.error(`Exit`));
 }
 
 newGame()
- 
+
