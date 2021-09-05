214a6d9f2a6a9ea20f2355a8ec5135e449d8ca0e
viktoriiadron <88160860+viktoriiadron@users.noreply.github.com>
2021-09-06 00:36:45 +0300

    done

---
+++ 31.Webpack,Classes/script.js
@@ -0,0 +1,74 @@
+import { students } from './students.js';
+
+function getOwnId() {
+    let id = 0;
+    return function () {
+        id += 1;
+        return id;
+        };
+}
+
+let getId = getOwnId();
+
+class Student {
+    constructor({ name, surname, ratingPoint, schoolPoint } = enrolle) {
+        this.name = name;
+        this.surname = surname;
+        this.ratingPoint = ratingPoint;
+        this.schoolPoint = schoolPoint;
+    }
+
+    static id = 1;
+
+    id = getId();
+    isSelfPayment = true;
+}
+
+class University {
+    constructor(name) {
+        this.name = name;
+    }
+    students = [];
+
+
+    addStudent(student) {
+        
+        this.students.push(student);
+        this.students.sort((a, b) =>
+            (a.ratingPoint + a.schoolPoint) - (b.ratingPoint + b.schoolPoint) 
+        ).reverse();
+       
+        for (let i = 0; i <= 4; i++) {
+            if (student.ratingPoint >= 800) {
+                newGroup[i].isSelfPayment = false; 
+            }
+        }
+    }
+
+    getAllStudents() {
+        return this.students;
+    }
+
+    getBugjetStudents() {
+        return this.students.filter(student => student.isSelfPayment === false);
+    }
+}
+
+let university = new University('KhNEU');
+
+
+
+let newGroup = students.map(function (enrolle) {
+    let newStudent = new Student(enrolle);
+    return newStudent;
+});
+
+newGroup.map(function (student) {
+    university.addStudent(student)
+} )
+
+console.log(newGroup);
+
+console.log(university.getAllStudents());
+console.log(university.getBugjetStudents());
+
